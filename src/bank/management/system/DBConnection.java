01afccb79b8430bff7cab129e7768cab0aef78fb
subhasis-sahoo <subhasissahoo949@gmail.com>
2025-04-29 08:05:15 +0530

    Establish DataBase Connection with MariaDb

---
+++ src/bank/management/system/DBConnection.java
@@ -0,0 +1,38 @@
+package bank.management.system;
+
+
+import java.sql.*;
+
+public class DBConnection {
+//    Five steps for JDBC connection
+//    Register the Driver
+//    Create connections
+//    Create statement
+//    Execute query
+//    Close connection
+
+    String url = "jdbc:mariadb://localhost:3306/bank_management_system";
+    String user = "root";
+    String password = "";
+
+    Connection conn;
+    Statement stmt;
+
+    public DBConnection() {
+        try {
+            // Load the MariaDB JDBC driver (optional in modern Java)
+            // Class.forName("org.mariadb.jdbc.Driver");
+
+            // Establish connection
+            conn = DriverManager.getConnection(url, user, password);
+
+            // Create statement
+            stmt = conn.createStatement();
+
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
